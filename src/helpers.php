4815b648a01efd89777fec53e11fdd188c9b5ca5
Usman Zahid <developerusman@yahoo.com>
2025-05-18 20:44:34 +0500

    Minor changes

--- src/helpers.php
+++ src/helpers.php
@@ -3,14 +3,14 @@
 use Usmanzahid\ServiceResponse\ServiceResponse;
 
 if (!function_exists('success')) {
-    function success(mixed $data = null, string $message = 'Success'): ServiceResponse {
+    function success(mixed $data = null, string $message = 'Success!'): ServiceResponse {
         return (new ServiceResponse())->withMessage($message)
             ->withData($data);
     }
 }
 
 if (!function_exists('fail')) {
-    function fail(array $errors = [], string $message = 'Failed'): ServiceResponse {
+    function fail(array $errors = [], string $message = 'Failed!'): ServiceResponse {
         return (new ServiceResponse())->withMessage($message)
             ->withSuccess(false)
             ->withErrors($errors);
