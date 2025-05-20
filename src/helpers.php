7255b148b99b3925053747e8f2b432f0170295e5
Usman Zahid <developerusman@yahoo.com>
2025-05-20 21:17:32 +0500

    Finalized

--- src/helpers.php
+++ src/helpers.php
@@ -2,17 +2,39 @@
 
 use Usmanzahid\ServiceResponse\ServiceResponse;
 
-if (!function_exists('success')) {
-    function success(mixed $data = null, string $message = 'Success!'): ServiceResponse {
-        return (new ServiceResponse())->withMessage($message)
+if (!function_exists('service_response_success')) {
+    function service_response_success(
+        mixed  $data = null,
+        string $message = 'Success!'
+    ): ServiceResponse {
+        return (new ServiceResponse())
+            ->withSuccess(true)
+            ->withMessage($message)
             ->withData($data);
     }
 }
 
-if (!function_exists('fail')) {
-    function fail(array $errors = [], string $message = 'Failed!'): ServiceResponse {
-        return (new ServiceResponse())->withMessage($message)
+if (!function_exists('service_response_fail')) {
+    function service_response_fail(
+        array  $errors = [],
+        string $message = 'Failed!',
+        mixed  $data = null
+    ): ServiceResponse {
+        return (new ServiceResponse())
             ->withSuccess(false)
-            ->withErrors($errors);
+            ->withMessage($message)
+            ->withErrors($errors)
+            ->withData($data);
+    }
+}
+
+if (!function_exists('service_response_from_exception')) {
+    function service_response_from_exception(
+        Throwable $e
+    ): ServiceResponse {
+        return service_response_fail(
+            ['exception' => [$e->getMessage()]],
+            $e->getCode(),
+        );
     }
 }
